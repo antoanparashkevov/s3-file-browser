8a4b9032551ed2efcfe0899fb1e966c6fdfce0ee
“antoanparashkevov” <tonkata1505@gmail.com>
2023-10-28 12:10:42 +0300

    Current directory actions arrows and button

--- src/components/currDirectory/CurrentDirectoryActions.tsx
+++ src/components/currDirectory/CurrentDirectoryActions.tsx
@@ -1,17 +1,34 @@
-import styles from "./CurrentDirectoryActions.module.scss";
 import React from "react";
+import styles from "./CurrentDirectoryActions.module.scss";
+import classNames from "classnames/bind";
+
+//UI components
 import { Button } from "../UI/BaseButton";
+
+const cx = classNames.bind(styles);
 
 const CurrentDirectoryActions = () => {
     
     return (
         <div className={styles['current_directory_actions']}>
             <div className={styles['current_directory_actions_navigate']}>
-               <img src="/icons/arrow.svg" alt="Prev Icon" width={24} height={24}/>
-               <img src="/icons/arrow.svg" alt="Next Icon" width={24} height={24}/>
+               <div className={cx({
+                        "arrow_wrapper": true,
+                        "arrow_prev": true,
+                    })}
+               >
+                   <img src="/icons/arrow.svg" alt="Prev Icon" width={24} height={24}/>
+               </div>
+                <div className={cx({
+                    "arrow_wrapper": true,
+                    "arrow_next": true,
+                })}
+                >
+                    <img src="/icons/arrow.svg" alt="Next Icon" width={24} height={24}/>
+                </div>
             </div>
             <div className={styles['current_directory_actions_create_delete']}>
-                <Button $primary $small>Create</Button>
+                <Button $small>Create</Button>
             </div>
         </div>
     )
